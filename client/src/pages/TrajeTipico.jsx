3bc02a631cb272d599dfb6f0c8317a7626f562ce
KernyMC <kalexandervargas@gmail.com>
2024-06-05 22:44:55 -0500

    aumento css

--- client/src/pages/TrajeTipico.jsx
+++ client/src/pages/TrajeTipico.jsx
@@ -8,6 +8,8 @@ import "./popup.scss";
 import Espera from "../components/Espera.jsx";
 import { API_BASE_URL } from "./ip";
 import Navbar from "../components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Dropdown from 'react-bootstrap/Dropdown';
 
 function Traje() {
@@ -194,10 +196,12 @@ function Traje() {
                                         src={require(`../candidatas/candidate${index + 1}.jpg`)}
                                         alt={`Candidate ${index + 1}`}
                                     />
+
                                     <div className="candidate-info">
                                        <span className="candidate-name">{index + 1}. {candidate.name}</span>
                                         <span className="candidate-career">{candidate.career}</span>
                                     </div>
+
                                     <div className="vote-button">
                                         <Dropdown onSelect={(eventKey) => handleScoreSelect(index + 1, eventKey)}>
                                             <Dropdown.Toggle variant="success" id="dropdown-basic">
